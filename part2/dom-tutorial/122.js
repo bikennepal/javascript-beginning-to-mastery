6624ac0eb8820b005e05c53e6a0e67c504a2cc5b
Biken <premilabhandari1@gmail.com>
2023-11-16 18:28:34 +0530

    adding more learning

--- part2/dom-tutorial/122.js
+++ part2/dom-tutorial/122.js
@@ -1,19 +1,23 @@
-const allButtons = document.querySelectorAll(".my-buttons button");
+const AllButton= document.querySelectorAll(".my-buttons button");
 
-
-// for(let button of allButtons){
-//     button.addEventListener("click", function(){
-//         console.log(this);
-//     })
-// }
-
-// for(let i = 0 ; i< allButtons.length; i++){
-//     allButtons[i].addEventListener("click", function(){
-//         console.log(this);
-//     })
-// }
-// allButtons.forEach(function(button){
-//     button.addEventListener("click", function(){
-//         console.log(this);
-//         });
+// firstButton.addEventListener("click",function(){
+//     console.log("you clicked me")
 // })
+
+// for(let button of AllButton){
+//     button.addEventListener("click", function(){
+//         console.log(this.textContent)
+//     })
+// }
+
+// for(let i=0; i<AllButton.length; i++){
+//     AllButton[i].addEventListener("click", function(){
+//         console.log(this);
+//     })
+// }
+
+AllButton.forEach(function(button){
+    button.addEventListener("click", function(){
+        console.log(this);
+    })
+})
